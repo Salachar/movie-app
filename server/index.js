78a64450c9f244b3d6ef89326f087c23adf44813
Salachar <salachar@gmail.com>
2021-05-24 15:56:11 -0700

    First commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// app.options('*', cors(corsOptions));
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
